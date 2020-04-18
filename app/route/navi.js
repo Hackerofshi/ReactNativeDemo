4ad0808531a7484dd7480a54280ceac299c25980
shixin <zhongyang.shi@uworks-gd.cc>
2020-04-18 16:19:05 +0800

    路由

--- app/route/navi.js
+++ app/route/navi.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
@@ -9,8 +9,14 @@ import {
     createStackNavigator
 } from 'react-navigation';
 
+import SectionListDemo from '../list/section_list'
 
 class NAV1 extends React.Component {
+
+    static navigationOptions = ({ navigation }) => ({
+        title: `练习`,
+    });
+
     render() {
         return (
             <View style={styles.container}>
@@ -23,10 +29,17 @@ class NAV1 extends React.Component {
                     }}
                 />
                 <Button
-                    style={styles.btn}
+                    style={{ marginTop: 10,paddingBottom:10 }}
                     title={'点击前往nav3'}
                     onPress={() => {
                         this.props.navigation.push('nav3')//push方法 跳转到
+                    }}
+                />
+                <Button
+                    style={{ marginTop: 10 }}
+                    title={'点击前往SectionListDemo'}
+                    onPress={() => {
+                        this.props.navigation.push('nav4')//push方法 跳转到
                     }}
                 />
             </View>
@@ -95,11 +108,14 @@ export default SimpleAppNavigator = createStackNavigator(
         },
         nav3: {
             screen: NAV3
+        },
+        nav4: {
+            screen: SectionListDemo
         }
     }, {
-        initialRouteName: 'nav1',//这个设置是初始化设置 初始化显示modul为nav1  可以尝试改变这个值为nav2
+    initialRouteName: 'nav1',//这个设置是初始化设置 初始化显示modul为nav1  可以尝试改变这个值为nav2
 
-    }
+}
 )
 
 // export default class Pages extends Component{
@@ -117,13 +133,21 @@ export default SimpleAppNavigator = createStackNavigator(
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexDirection:'column',
         alignItems: 'center',
         justifyContent: 'center',
     },
     text: {
         color: 'red',
+        padding: 10,
         backgroundColor: 'pink',
         fontSize: 25,
+        marginBottom: 10
     },
+    btn: {
+        marginTop: 10,
+        paddingBottom: 10,
+        marginBottom: 10,
+
+    }
 })
--- index.js
+++ index.js
@@ -11,48 +11,7 @@ import SimpleAppNavigator from './app/route/navi'
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import React, { Component } from 'react';
 
-class HomeScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Welcome',
-        headerStyle:{
-            backgroundColor:'#00a0e9'
-        },
-        headerTintColor:'white',
-    };
-    render() {
-        const { navigate } = this.props.navigation;
-        return <View>
-            <Text>Hello, Chat App!</Text>
-            <Button
-                onPress={() => navigate('Chat', { user: 'Lucy' })}
-                title="Chat with Lucy"
-            />
-        </View>;
-    }
-}
 
-
-
-class ChatScreen extends React.Component {
-    // Nav options can be defined as a function of the screen's props:
-    static navigationOptions = ({ navigation }) => ({
-        title: `Chat with ${navigation.state.params.user}`,
-    });
-    render() {
-        // The screen's current route is passed in to `props.navigation.state`:
-        const { params } = this.props.navigation.state;
-        return (
-            <View>
-                <Text>Chat with Lucy</Text>
-            </View>
-        );
-    }
-}
-
-const SimpleApp = createStackNavigator({
-    Home: { screen: HomeScreen },
-    Chat: { screen: ChatScreen },
-});
 
 
 AppRegistry.registerComponent(appName, () => createAppContainer(SimpleAppNavigator));
