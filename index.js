549b95caea96d3a47473e369e2e8ae408b44bd1c
shixin <zhongyang.shi@uworks-gd.cc>
2020-04-18 16:39:10 +0800

    提交

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
 import { AppRegistry, Text, View,Button } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import LearnRN from './app/fun_demo'
-import FlexDemo from './app/flex_demo'
-import SimpleAppNavigator from './app/route/navi'
+import LearnRN from './lib/fun_demo'
+import FlexDemo from './lib/flex_demo'
+import SimpleAppNavigator from './lib/route/navi'
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import React, { Component } from 'react';
 
