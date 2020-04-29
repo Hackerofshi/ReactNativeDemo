bd988257d3c41df5e4d0993149199c42fbbd5c31
shixin <zhongyang.shi@uworks-gd.cc>
2020-04-29 18:04:16 +0800

    细节的修改

--- App.Demo.js
+++ App.Demo.js
@@ -6,60 +6,61 @@
  * @flow
  */
 
-import React from 'react'
+import React from 'react';
 import {
   ActivityIndicator,
   AsyncStorage,
   View,
   StatusBar,
   Button,
-} from 'react-native'
+} from 'react-native';
 
 import {
   createStackNavigator,
   createAppContainer,
   createSwitchNavigator,
-} from 'react-navigation'
+} from 'react-navigation';
 
-import MainScreen from './lib/ui/main'
-import SignInScreen from './lib/ui/login/login_page'
-import ForgetPasswordScreen from './lib/ui/login/forget_password'
-import RegisterScreen from './lib/ui/login/register'
+import MainScreen from './lib/ui/main';
+import SignInScreen from './lib/ui/login/login_page';
+import ForgetPasswordScreen from './lib/ui/login/forget_password';
+import RegisterScreen from './lib/ui/login/register';
 
 class AuthLoadingScreen extends React.Component {
   constructor(props) {
     super(props);
     this._bootstrpsAsyns();
-
   }
 
   _bootstrpsAsyns = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
     this.props.navigation.navigate(userToken ? 'App' : 'Auth');
-  }
+  };
   render() {
-    return (<View>
-      <ActivityIndicator />
-      <StatusBar barStyle='default' />
-    </View>)
+    return (
+      <View>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
+    );
   }
 }
 
-
 const LoginStack = createStackNavigator({
-  Login: { screen: SignInScreen },
-  SetPW: { screen: ForgetPasswordScreen },
-  Register: { screen: RegisterScreen }
-})
+  Login: {screen: SignInScreen},
+  SetPW: {screen: ForgetPasswordScreen},
+  Register: {screen: RegisterScreen},
+});
 
-
-export default createAppContainer(createSwitchNavigator(
-  {
-    AuthLoading: AuthLoadingScreen,
-    App: MainScreen,
-    Auth: LoginStack
-  },
-  {
-    initialRouteName: 'AuthLoading',
-  })
-)
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingScreen,
+      App: MainScreen,
+      Auth: LoginStack,
+    },
+    {
+      initialRouteName: 'AuthLoading',
+    },
+  ),
+);
